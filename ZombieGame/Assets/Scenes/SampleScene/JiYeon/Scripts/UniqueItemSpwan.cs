a1d81e735677e206474618644df3fb1543417a16
Jiyeon! <101547672+Majeokdoo@users.noreply.github.com>
2022-05-10 14:49:53 +0900

    new palette change~

---
+++ ZombieGame/Assets/Scenes/SampleScene/JiYeon/Scripts/UniqueItemSpwan.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UniqueItemSpawn : MonoBehaviour
+{
+    public GameObject objectToCreate;
+    public List<ItemSpawn> placesCanCreate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Pick a random room
+        ItemSpawn RandomPlace = placesCanCreate[Random.Range(0, placesCanCreate.Count)];
+        Vector3 RandomPos = RandomPlace.GetRandomPosition();
+        Instantiate(objectToCreate, RandomPos, Quaternion.identity);
+
+        print("An unique Item is here!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
